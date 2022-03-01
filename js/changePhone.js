e4bfb8ce433b45112bffba7cd0435ab68ad0f401
Artur Santos <artur_teixeira10@hotmail.com>
2022-03-01 12:36:44 -0300

    code

--- js/changePhone.js
+++ js/changePhone.js
@@ -4,7 +4,7 @@ function getUrlImgS22(srcImgS22, corCelularS22) {
 
     let celular_s22 = document.getElementById('celular_s22')
     
-    s_22.src = `../imagens/s22/${srcImgS22}`
+    s_22.src = `../imagens/${srcImgS22}`
 
     celular_s22.style.background = corCelularS22
     
@@ -16,7 +16,7 @@ function getUrlImgS22plus(srcImgS22plus, corCelularS22Plus) {
 
     let celular_s22_plus = document.getElementById('celular_s22_plus')
     
-    s_22_plus.src = `../imagens/s22_plus/${srcImgS22plus}`
+    s_22_plus.src = `../imagens/${srcImgS22plus}`
 
     celular_s22_plus.style.background = corCelularS22Plus
     
@@ -28,7 +28,7 @@ function getUrlImgS22ultra(srcImgS22ultra, corCelularS22Ultra) {
 
     let celular_s22_ultra = document.getElementById('celular_s22_ultra')
     
-    s_22_ultra.src = `../imagens/s22_ultra/${srcImgS22ultra}`
+    s_22_ultra.src = `../imagens/${srcImgS22ultra}`
 
     celular_s22_ultra.style.background = corCelularS22Ultra
     
